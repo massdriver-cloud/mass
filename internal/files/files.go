1e049c3600bcd4141cf1377cecfb81ac7ca5a1b1
Cory O'Daniel <cory@coryodaniel.com>
2022-12-12 16:39:12 -0800

    check ext for file handling

--- internal/files/files.go
+++ internal/files/files.go
@@ -2,32 +2,46 @@ package files
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
 )
 
 const USER_RW = 0600
 
 func Write(path string, data interface{}) error {
-	// TODO: check file type first
-	json, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
-		return err
+	var formattedData []byte
+	ext := filepath.Ext(path)
+
+	switch ext {
+	case ".json":
+		json, err := json.MarshalIndent(data, "", "  ")
+		formattedData = json
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("unsupported file type: %s", ext)
 	}
 
-	return os.WriteFile(path, json, USER_RW)
+	return os.WriteFile(path, formattedData, USER_RW)
 }
 
 func Read(path string, v any) error {
+	ext := filepath.Ext(path)
+
 	contents, err := os.ReadFile(path)
 	if err != nil {
 		return err
 	}
 
-	// TODO: check file type first
-	err = json.Unmarshal(contents, &v)
-
-	if err != nil {
-		return err
+	switch ext {
+	case ".json":
+		if err := json.Unmarshal(contents, &v); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("unsupported file type: %s", ext)
 	}
 
 	return nil
