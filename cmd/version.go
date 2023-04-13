a885417844c21dd35c6a9321e0bede14a7e12e3f
mclacore <michael@massdriver.cloud>
2023-04-13 15:21:05 -0600

    Saving version updates, still troubleshooting

--- cmd/version.go
+++ cmd/version.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/massdriver-cloud/mass/internal/prettylogs"
 	"github.com/massdriver-cloud/mass/internal/version"
 	"github.com/spf13/cobra"
 )
@@ -17,13 +18,15 @@ var versionCmd = &cobra.Command{
 }
 
 func runVersion(cmd *cobra.Command, args []string) {
+
 	isOld, latestVersion, err := version.CheckForNewerVersionAvailable()
 	if err != nil {
 		fmt.Printf("could not check for newer versions: %v. skipping...\n", err.Error())
 	} else if isOld {
 		fmt.Printf("A newer version of the CLI is available, you can download it here: %v\n", version.LatestReleaseURL)
 	}
-	fmt.Printf("Mass CLI version: %v\n", latestVersion)
+	var outputColor = prettylogs.Green(latestVersion)
+	fmt.Printf("Mass CLI version: %v\n", outputColor)
 }
 
 func init() {
