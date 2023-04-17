4eb233141cee8fb1cebbc7b689b7ba19a5b45c87
Michael Lacore <michael@massdriver.cloud>
2023-04-17 11:15:13 -0600

    Adding suggested changes
    Co-authored-by: Christopher Hill <chris@massdriver.cloud>

--- cmd/version.go
+++ cmd/version.go
@@ -14,19 +14,20 @@ var versionCmd = &cobra.Command{
 	Aliases: []string{"v"},
 	Short:   "Version of Mass CLI",
 	Long:    ``,
-	Run:     runVersion,
+	RunE:     runVersion,
 }
 
-func runVersion(cmd *cobra.Command, args []string) {
+func runVersion(cmd *cobra.Command, args []string) error {
 
 	isOld, latestVersion, err := version.CheckForNewerVersionAvailable()
 	if err != nil {
-		fmt.Printf("could not check for newer versions: %v. skipping...\n", err.Error())
+		return fmt.Errorf("could not check for newer versions: %w. skipping...\n", err)
 	} else if isOld {
 		fmt.Printf("A newer version of the CLI is available, you can download it here: %v\n", version.LatestReleaseURL)
 	}
 	var outputColor = prettylogs.Green(latestVersion)
 	fmt.Printf("Mass CLI version: %v\n", outputColor)
+return nil
 }
 
 func init() {
