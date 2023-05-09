f71140a0fc3ad78046edb6c0fa481448af6f7395
Dave Williams <xpositivityx@gmail.com>
2023-05-09 11:49:00 -0700

    adding gql queries for get artifact definitions and create artifact (#34)
    * adding gql queries for get artifact definitions and create artifact
    
    * Fix test
    
    ---------
    
    Co-authored-by: chrisghill <chris@massdriver.cloud>

---
+++ internal/api/artifact_defintions.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"context"
+
+	"github.com/Khan/genqlient/graphql"
+)
+
+func GetArtifactDefinitions(client graphql.Client, orgID string, input ArtifactDefinitionInput) ([]ArtifactDefinitionWithSchema, error) {
+	response, err := getArtifactDefinitions(context.Background(), client, orgID, input)
+	return response.toArtifactDefinitions(), err
+}
+
+func (ad *getArtifactDefinitionsResponse) toArtifactDefinitions() []ArtifactDefinitionWithSchema {
+	var ads []ArtifactDefinitionWithSchema
+	for _, artifactDefinition := range ad.ArtifactDefinitions {
+		ads = append(ads, ArtifactDefinitionWithSchema(artifactDefinition))
+	}
+
+	return ads
+}
