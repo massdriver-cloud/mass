1efa41772929b56c972037fc8ae06d1a488820d8
Michael Lacore <michael@massdriver.cloud>
2023-05-09 01:23:12 -0600

    Adding version command (#33)
    * Adding version command
    
    * Saving version updates, still troubleshooting
    
    * trying to expand tests
    
    * Adding suggested changes
    
    Co-authored-by: Christopher Hill <chris@massdriver.cloud>
    
    * bug fixes
    
    ---------
    
    Co-authored-by: Christopher Hill <chris@massdriver.cloud>

--- internal/prettylogs/main.go
+++ internal/prettylogs/main.go
@@ -5,3 +5,7 @@ import "github.com/charmbracelet/lipgloss"
 func Underline(word string) lipgloss.Style {
 	return lipgloss.NewStyle().SetString(word).Underline(true).Foreground(lipgloss.Color("#7D56f4"))
 }
+
+func Green(word string) lipgloss.Style {
+	return lipgloss.NewStyle().SetString(word).Foreground(lipgloss.Color("#00FF00"))
+}
