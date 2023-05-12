6ba7f84abc4eadb91082c60cf83eaf03ba8418b0
dwill <xpositivityx@gmail.com>
2023-05-12 17:24:35 +0000

    adding test for artifact_import

--- internal/commands/artifact_import_test.go
+++ internal/commands/artifact_import_test.go
@@ -1,44 +1,65 @@
 package commands_test
 
-// func TestArtifactImport(t *testing.T) {
-// 	responses := []interface{}{
-// 		gqlmock.MockQueryResponse("getArtifactDefinitions", []api.ArtifactDefinitionWithSchema{
-// 			{
-// 				Name: "massdriver/fake-artifact-schema",
-// 				Schema: map[string]interface{}{
-// 					"$id":     "id",
-// 					"$schema": "http://json-schema.org/draft-07/schema",
-// 					"type":    "object",
-// 					"properties": map[string]interface{}{
-// 						"name": map[string]interface{}{
-// 							"type": "string",
-// 						},
-// 					},
-// 				},
-// 			},
-// 		}),
-// 		gqlmock.MockMutationResponse("createArtifact", api.Artifact{
-// 			ID:   "artifact-id",
-// 			Name: "artifact-name",
-// 		}),
-// 	}
+import (
+	"testing"
 
-// 	client := gqlmock.NewClientWithJSONResponseArray(responses)
+	"github.com/massdriver-cloud/mass/internal/commands"
+	"github.com/massdriver-cloud/mass/internal/gqlmock"
+	"github.com/spf13/afero"
+)
 
-// 	var fs = afero.NewMemMapFs()
+func TestArtifactImport(t *testing.T) {
+	client := gqlmock.NewClientWithJSONResponseMap(map[string]interface{}{
+		"getArtifactDefinitions": map[string]interface{}{
+			"data": map[string]interface{}{
+				"artifactDefinitions": []map[string]interface{}{
+					{
+						"name": "massdriver/fake-artifact-schema",
+						"schema": map[string]interface{}{
+							"$id":     "id",
+							"$schema": "http://json-schema.org/draft-07/schema",
+							"type":    "object",
+							"properties": map[string]interface{}{
+								"name": map[string]interface{}{
+									"type": "string",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"createArtifact": map[string]interface{}{
+			"data": map[string]interface{}{
+				"createArtifact": map[string]interface{}{
+					"result": map[string]interface{}{
+						"id":   "artifact-id",
+						"name": "artifact-name",
+					},
+					"successful": true,
+				},
+			},
+		},
+	})
 
-// 	file, err := fs.Create("artifact.json")
-// 	checkErr(err, t)
+	var fs = afero.NewMemMapFs()
 
-// 	file.Write([]byte(`{"name":"fake"}`))
+	file, err := fs.Create("artifact.json")
 
-// 	got, err := commands.ArtifactImport(client, "faux-org-id", fs, "artifact-name", "massdriver/fake-artifact-schema", "artifact.json")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	want := "artifact-id"
-// 	if got != want {
-// 		t.Errorf("got %s , wanted %s", got, want)
-// 	}
-// }
+	file.Write([]byte(`{"name":"fake"}`))
+
+	got, err := commands.ArtifactImport(client, "faux-org-id", fs, "artifact-name", "massdriver/fake-artifact-schema", "artifact.json")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "artifact-id"
+	if got != want {
+		t.Errorf("got %s , wanted %s", got, want)
+	}
+}
