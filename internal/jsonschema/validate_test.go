50d34369f90abd4a4bde3117dfab6f189e4e1913
Cory O'Daniel <cory@coryodaniel.com>
2023-08-24 13:20:16 -0700

    Add schema validate cmd
    We use this in our own CI. It currently fails locally using the new cli.
    
    We can now update our CI pipelines to use the new CLI... woohoo

---
+++ internal/jsonschema/validate_test.go
@@ -0,0 +1,43 @@
+package jsonschema_test
+
+import (
+	"testing"
+
+	"github.com/massdriver-cloud/mass/internal/jsonschema"
+)
+
+func TestValidate(t *testing.T) {
+	type test struct {
+		name         string
+		schemaPath   string
+		documentPath string
+		want         bool
+	}
+	tests := []test{
+		{
+			name:         "ValidDocument",
+			schemaPath:   "testdata/valid-schema.json",
+			documentPath: "testdata/valid-document.json",
+			want:         true,
+		},
+		{
+			name:         "InvalidDocument",
+			schemaPath:   "testdata/valid-schema.json",
+			documentPath: "testdata/invalid-document.json",
+			want:         false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := jsonschema.Validate(tc.schemaPath, tc.documentPath)
+			if err != nil {
+				t.Errorf("Error during validation: %s", err)
+			}
+
+			if got.Valid() != tc.want {
+				t.Errorf("got %t want %t", got.Valid(), tc.want)
+			}
+		})
+	}
+}
