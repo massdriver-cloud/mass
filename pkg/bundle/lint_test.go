53b2f1d9f6562d55e41d76369601f38601ec0dde
mclacore <michael@massdriver.cloud>
2024-06-18 15:29:55 -0700

    uncommenting tests

--- pkg/bundle/lint_test.go
+++ pkg/bundle/lint_test.go
@@ -265,80 +265,78 @@ func TestLintMatchRequired(t *testing.T) {
 		err  error
 	}
 	tests := []test{
-		// {
-		// 	name: "Valid pass",
-		// 	bun: &bundle.Bundle{
-		// 		Name:        "example",
-		// 		Description: "description",
-		// 		Access:      "private",
-		// 		Schema:      "draft-07",
-		// 		Type:        "infrastructure",
-		// 		Params:      map[string]interface{}{
-		// 			"required": []interface{}{"foo"},
-		// 			"properties": map[string]interface{}{
-		// 				"foo": map[string]interface{}{
-		// 					"type": "string",
-		// 				},
-		// 			},
-		// 		},
-		// 		Connections: map[string]interface{}{},
-		// 		Artifacts:   map[string]interface{}{},
-		// 		UI:          map[string]interface{}{},
-		// 	},
-		// 	err: nil,
-		// },
-		// {
-		// 	name: "Invalid missing param",
-		// 	bun: &bundle.Bundle{
-		// 		Name:        "example",
-		// 		Description: "description",
-		// 		Access:      "private",
-		// 		Schema:      "draft-07",
-		// 		Type:        "infrastructure",
-		// 		Params:      map[string]interface{}{
-		// 			"required": []interface{}{"bar"},
-		// 			"properties": map[string]interface{}{
-		// 				"foo": map[string]interface{}{
-		// 					"type": "string",
-		// 				},
-		// 			},
-		// 		},
-		// 		Connections: map[string]interface{}{},
-		// 		Artifacts:   map[string]interface{}{},
-		// 		UI:          map[string]interface{}{},
-		// 	},
-		// 	err: errors.New("required parameter bar is not defined in properties"),
-		// },
-		// {
-		// 	name: "Nested valid test",
-		// 	bun: &bundle.Bundle{
-		// 		Name:        "example",
-		// 		Description: "description",
-		// 		Access:      "private",
-		// 		Schema:      "draft-07",
-		// 		Type:        "infrastructure",
-		// 		Params:      map[string]interface{}{
-		// 			"required": []interface{}{"foo"},
-		// 			"properties": map[string]interface{}{
-		// 				"foo": map[string]interface{}{
-		// 					"type": "object",
-		// 					"properties": map[string]interface{}{
-		// 						"required": []interface{}{"bar"},
-		// 						"properties": map[string]interface{}{
-		// 							"bar": map[string]interface{}{
-		// 								"type": "string",
-		// 							},
-		// 						},
-		// 					},
-		// 				},
-		// 			},
-		// 		},
-		// 		Connections: map[string]interface{}{},
-		// 		Artifacts:   map[string]interface{}{},
-		// 		UI:          map[string]interface{}{},
-		// 	},
-		// 	err: nil,
-		// },
+		{
+			name: "Valid pass",
+			bun: &bundle.Bundle{
+				Name:        "example",
+				Description: "description",
+				Access:      "private",
+				Schema:      "draft-07",
+				Type:        "infrastructure",
+				Params: map[string]interface{}{
+					"required": []interface{}{"foo"},
+					"properties": map[string]interface{}{
+						"foo": map[string]interface{}{
+							"type": "string",
+						},
+					},
+				},
+				Connections: map[string]interface{}{},
+				Artifacts:   map[string]interface{}{},
+				UI:          map[string]interface{}{},
+			},
+			err: nil,
+		},
+		{
+			name: "Invalid missing param",
+			bun: &bundle.Bundle{
+				Name:        "example",
+				Description: "description",
+				Access:      "private",
+				Schema:      "draft-07",
+				Type:        "infrastructure",
+				Params: map[string]interface{}{
+					"required": []interface{}{"bar"},
+					"properties": map[string]interface{}{
+						"foo": map[string]interface{}{
+							"type": "string",
+						},
+					},
+				},
+				Connections: map[string]interface{}{},
+				Artifacts:   map[string]interface{}{},
+				UI:          map[string]interface{}{},
+			},
+			err: errors.New("required parameter bar is not defined in properties"),
+		},
+		{
+			name: "Nested valid test",
+			bun: &bundle.Bundle{
+				Name:        "example",
+				Description: "description",
+				Access:      "private",
+				Schema:      "draft-07",
+				Type:        "infrastructure",
+				Params: map[string]interface{}{
+					"required": []interface{}{"foo"},
+					"properties": map[string]interface{}{
+						"foo": map[string]interface{}{
+							"type":     "object",
+							"required": []interface{}{"bar"},
+							"properties": map[string]interface{}{
+								"bar": map[string]interface{}{
+									"type": "string",
+								},
+							},
+						},
+					},
+				},
+				Connections: map[string]interface{}{},
+				Artifacts:   map[string]interface{}{},
+				UI:          map[string]interface{}{},
+			},
+			err: nil,
+		},
 		{
 			name: "Nested invalid test",
 			bun: &bundle.Bundle{
