c6344594ef2312457320a9f19d78fb8668c93992
Christopher Hill <chris@massdriver.cloud>
2026-07-31 10:35:40 -1000

    Use pointer semantics for entity update inputs (SDK v0.2.15) (#248)
    * bump SDK dependency
    
    * fix lint errors

--- cmd/environment.go
+++ cmd/environment.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"embed"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -423,28 +424,21 @@ func runEnvironmentUpdate(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("error initializing massdriver client: %w", err)
 	}
 
-	current, err := mdClient.Environments.Get(ctx, environmentID)
-	if err != nil {
-		return fmt.Errorf("error getting environment: %w", err)
+	if !cmd.Flags().Changed("name") && !cmd.Flags().Changed("description") && !cmd.Flags().Changed("attributes") {
+		return errors.New("nothing to update: set at least one of --name, --description, or --attributes")
 	}
 
-	if !cmd.Flags().Changed("name") {
-		name = current.Name
+	// Only send fields whose flags were explicitly set. Nil pointers (and a
+	// nil Attributes map) leave the corresponding values unchanged at the server.
+	input := environments.UpdateInput{}
+	if cmd.Flags().Changed("name") {
+		input.Name = &name
 	}
-	if !cmd.Flags().Changed("description") {
-		description = current.Description
+	if cmd.Flags().Changed("description") {
+		input.Description = &description
 	}
-	var attributes map[string]any
 	if cmd.Flags().Changed("attributes") {
-		attributes = cli.AttributesToAnyMap(attrs)
-	} else {
-		attributes = current.Attributes
-	}
-
-	input := environments.UpdateInput{
-		Name:        name,
-		Description: description,
-		Attributes:  attributes,
+		input.Attributes = cli.AttributesToAnyMap(attrs)
 	}
 
 	updated, err := mdClient.Environments.Update(ctx, environmentID, input)
